2d4be35c72592bc9e569494129721941f23824be
Olga Matsyk <olga.matsyk2016@gmail.com>
2019-05-19 23:58:20 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Olga Matsyk on 5/19/19.
+//  Copyright © 2019 Matsyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
